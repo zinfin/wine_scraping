44aab2f18b05ef56c817021398fa2eb4b8df4d02
Sandie Peters <sandiepeters@concepcions-mbp.attlocal.net>
2017-05-21 17:30:12 -0700

    Fish for parenet div, extract div class and find all

--- wine/wine_scrapy/wine_scrapy/spiders/wine_spider.py
+++ wine/wine_scrapy/wine_scrapy/spiders/wine_spider.py
@@ -9,7 +9,7 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.cakewines.com/collections/all'
+            'http://www.owenroe.com/Our-Wine/White-Wines'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -25,11 +25,29 @@ class QuotesSpider(scrapy.Spider):
         for wine in grapes:
             results = soup.find_all(string=re.compile('%s' % wine, re.IGNORECASE))
             if (len(results) > 0):
-                print results
+                classDivName = self.getParentDivClassName(results[0])
+                print classDivName
+                self.getWineDivs(soup, classDivName)
                 break
         if len(results)==0:
             print "No wines found"   
 
+    # Find the class name of the div for a found wine
+    def getParentDivClassName(self, result):
+        classname = None
+        parentDiv = result.find_parent("div")
+        attributeDict = parentDiv.attrs
+        # Grab the first value in the dictionary for the class key
+        if(attributeDict["class"] != None):
+            classNames = attributeDict["class"]
+            classname = classNames[0]
+        return classname;
+
+    # Get all the divs with the class name
+    def getWineDivs(self, soup, classname):
+        wines = soup.find_all(attrs={"class":'%s' % classname})
+        for wine in wines:
+            print wine
     # Load the file of grape varietals, returning a list
     def loadVarietals(self):
         wineFile = "wine_scrapy/spiders/grape_varietals"
